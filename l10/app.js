1aac511e1270afd3b08eb4d874c65a6ef7921b8f
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2025-02-03 13:54:55 +0500

    MIDDLEWARE

--- l10/app.js
+++ l10/app.js
@@ -8,8 +8,16 @@ const app=express();
 // app.put 
 // app.delete
 
-app.get("/home",(req,res)=>{
+app.get("/api/v1/user/home",(req,res)=>{
     res.send("<h1>home page</h1>");
+    res.send(user);
+})
+app.get("/api/v1/user/about",(req,res)=>{
+    res.send("<h1>about page</h1>");
+    res.send(user);
+})
+app.get("/api/v1/user/contactus",(req,res)=>{
+    res.send("<h1>contact us page</h1>");
     res.send(user);
 })
 app.get("/api/v1/user/profile",(req,res)=>{
@@ -19,8 +27,22 @@ app.get("/api/v1/user/profile",(req,res)=>{
             username:"aaqib",
             email:"aaqib@hgmail.com"
         }
-    })
-})
+    });
+});
+app.get("/api/v1/product/:productId", (req, res) => {
+    const id = req.params.productId;
+    console.log("Product ID:", id);
+
+    const product = {
+        id: id, 
+        name: "MacBook",
+    };
+
+    res.status(200).json({
+        success: true,
+        product,
+    });
+});
 app.listen(8000,()=>{
     console.log("server listen at port 8000");
 });
