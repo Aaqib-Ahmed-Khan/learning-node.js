6387f842c193b350c8525cfb4406461dbaf3c509
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2025-02-03 13:12:06 +0500

    pages making using express

--- l10/app.js
+++ l10/app.js
@@ -1,8 +1,28 @@
 const express=require("express");
 //1 step to create web server using express
+
 const app=express();
+// app.post
+// app.get
+// app.patch
+// app.put 
+// app.delete
+
+app.get("/home",(req,res)=>{
+    res.send("<h1>home page</h1>");
+    res.send(user);
+})
+app.get("/api/v1/user/profile",(req,res)=>{
+    res.status(200).json({
+        success:true,
+        user:{
+            username:"aaqib",
+            email:"aaqib@hgmail.com"
+        }
+    })
+})
 app.listen(8000,()=>{
-    console.log("server lsiten at port 8000");
+    console.log("server listen at port 8000");
 });
 
 
